cbea04fd7336a689d0309ce1a63cb2c452071b88
khani <mojtaba.khani@yahoo.com>
2017-10-09 12:01:51 +0330

    update vendors

--- config/XBtelegram.php
+++ config/XBtelegram.php
@@ -5,4 +5,21 @@ return [
     'bot-token'=>'448314040:AAEk7o1F_VMgQ3T2MQMeysio8UK6VFu9Q_Y',
     'bot-username'=>'botesterbot',
     'meeting-storage'=>'file',
+    'callbackQueryJsonFormat'=>true,
+
+
+    ///> tenant area
+    'tenant'=>[
+        // 'mysql database name'=>[                                ///> [Optional] target
+        //     'message'=>'insert mysql username',                 ///> question or message for get a value
+        //     'field'=>'database.connections.mysql.username',     ///> path of config
+        //     'default'=>'root',                                  ///> [Optional] default value
+        // ],
+
+        'database name'=>[
+            'message'=>'insert token of new bot',
+            'field'=>'XBtelegram.bot-token',
+            'default'=>'215119625:AAGEbfmB-YDedqPIBGdcs6euPU6ro3eSlmc',
+        ],
+    ],
 ];
