57290a9588e6eee098afbb3a5755a63437f02821
khani <mojtaba.khani@yahoo.com>
2017-10-14 19:00:33 +0330

    redesigen category keyboard layout and logic

--- app/Magazines/Categories.php
+++ app/Magazines/Categories.php
@@ -5,36 +5,25 @@ namespace App\Magazines;
 use XB\theory\Magazine;
 use App\Model\Category;
 use XB\telegramMethods\sendMessage;
+use XB\telegramMethods\editMessageText;
 
 class Categories extends Magazine{
     public function index(){
-        $categories = Category::get(['id','name']);
+        $categories = Category::get(['name','id'])->toarray();
 
-        $cat_names = [];
-
-        foreach($categories->toArray() as $category){
-           $callback = [
-                "cat"=>(string)$category['id'],
-                "goto"=>"Products@index"
-            ]; 
-           array_push($cat_names,[['text'=>$category['name'],"callback_data"=> json_encode($callback)]]);
-        }
-        
-
-        $replyMarkup = [
-            'inline_keyboard' => 
-                $cat_names
-            
-        ];
-        $encodedMarkup = json_encode($replyMarkup);
-
-//  dd($encodedMarkup);
-        $send=new sendMessage([
+        $message=[
             'chat_id'=>$this->detect->from->id,
             'text'=>"لطفا یک دسته انتخاب کنید.",
             'parse_mode'=>'html',
-            'reply_markup'=>$encodedMarkup
-            ]);
+            'reply_markup'=>view('categoryKeyboard',['items'=>$categories])->render()
+        ];
+
+        $api=sendMessage::class;
+        if($this->detect->type=='callback_query'){
+            $api=editMessageText::class;
+            $message['message_id']=$this->update->callback_query->message->message_id;
+        }
+        $send=new $api($message);
         $send();
     }
     
