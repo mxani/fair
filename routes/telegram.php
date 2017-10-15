06e5be1f92607906a719cfd946858e344dde2147
khani <mojtaba.khani@yahoo.com>
2017-10-15 10:10:03 +0330

    summarize bot trigges

--- routes/telegram.php
+++ routes/telegram.php
@@ -3,7 +3,7 @@ use XB\theory\Shoot;
 
 
 
-if(!empty($this->update->message->text) && !empty($this->update->message->from)){
+if($this->detect->type=='message'){
     
         $this->trigger(function(){
         return $this->update->message->text == '/start';
@@ -28,25 +28,4 @@ if(!empty($this->update->message->text) && !empty($this->update->message->from))
 }
 
 
-// $this->trigger(function(){return true;},'sayHello');
- 
-
-
-$this->trigger(function($u){
-    return !empty($u->message->text) && $u->message->text=='get file';
-},function(){
-    $send=new XB\telegramMethods\sendPhoto([
-        'chat_id'=>$this->detect->from->id,
-        'photo'=>'/home/xani/Pictures/Man-Brain-415x347.jpg',
-    ]);
-    $send('photo');
-    $m['fileid']=$send->result->photo[0]->file_id;
-});
-
-$this->trigger('default',function() {
-    $send=new \XB\telegramMethods\sendMessage([
-        'chat_id'=>$this->detect->from->id,
-        'text'=>"what is your mean?? 🤔\n try again!!",
-    ]);
-    $send();
-});
+$this->trigger('default','sayHello');
