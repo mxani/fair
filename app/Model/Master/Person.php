7891e574b6d58b4a9abbe6ee99209c49e559e661
khani <mojtaba.khani@yahoo.com>
2017-10-17 14:09:08 +0330

    person entity added  (in master)

---
+++ app/Model/Master/Person.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model\Master;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Person extends Model
+{
+    use SoftDeletes;
+    protected $fillable = [ 'telegramID','detail','type','status'];
+    protected $dates = [ 'created_at', 'updated_at', 'deleted_at' ];
+    protected $hidden = [ 'updated_at', 'deleted_at' ];
+    protected $casts = ['detail' => 'array'];
+}
