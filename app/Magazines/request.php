1b1b845effbb6b27936884df8d253579e6bf5fcf
khani <mojtaba.khani@yahoo.com>
2017-10-25 17:46:23 +0330

    bug fix: forgotten Goto mechanism

--- app/Magazines/request.php
+++ app/Magazines/request.php
@@ -8,19 +8,38 @@ use App\Model\Person;
 
 class request extends Magazine{
     public function contact(){
-        $this->meet['request']['getContact']=1;
+        $this->meet['goto']='request@contactSave';
         $send=new sendMessage([
             'chat_id'=>$this->detect->chat->id??$this->detect->from->id,
             'text'=>"برای ادامه فرآیند, داشتن شماره تلفن همراه شما الزامی میباشد.\nشماره تلفن شما قبلن ثبت نشده است.\nبا فشوردن دکمه زیر شماره تلفن شما بطور خودکار ثبت میشود.\nاین تضمین به شما داده میشود که شماره تلفن شما مورد هیچگونه سؤ استفاده قرار نگیرد",
             'parse_mode'=>'html',
-            'reply_markup'=>'{"keyboard":[[{"text":"دراختیار قرار دادن شماره تلفن","request_contact":true},{"text":"انصراف"}]],"resize_keyboard":true,"one_time_keyboard":true}'
+            'reply_markup'=>$this->keyboard,
             ]);
         $send();
     }
 
+    public function invalidContact(){
+        $this->meet['goto']='request@contactSave';
+        $send=new sendMessage([
+            'chat_id'=>$this->detect->chat->id??$this->detect->from->id,
+            'text'=>"اطلاعات نادرست\nفقط از دکمه زیر برای وارد کردن شماره تلفن استفاده کنید.",
+            'parse_mode'=>'html',
+            'reply_markup'=>$this->keyboard,
+            ]);
+        $send();
+    }
+
+    private $keyboard='{"keyboard":[[{"text":"دراختیار قرار دادن شماره تلفن (خودکار)","request_contact":true}],[{"text":"انصراف"}]],"resize_keyboard":true,"one_time_keyboard":true}';
+
     public function contactSave(){
-        if($this->update->message->contact->user_id!=$this->detect->from->id){
-            $this->contact();
+        $user_id=$this->update->message->contact->user_id??$this->update->message->text??null;
+        if($user_id=='انصراف'){
+            $this->caller(mstGreet::class)->mainMenu();
+            unset($this->meet['request']);        
+            return;
+        }
+        if($user_id!=$this->detect->from->id){
+            $this->invalidContact();
             return;
         }
         $person=Person::where('telegramID',$this->detect->from->id)->first();
@@ -30,8 +49,8 @@ class request extends Magazine{
         $person->save();
         $mag='App\Magazines\\'.$this->meet['request']['ref_mag'];
         $car=$this->meet['request']['ref_car']??'main';
+        unset($this->meet['request']);
         $this->caller($mag)->$car();
-        unset($this->meet['request']['ref_mag']);
     }
     
 }
