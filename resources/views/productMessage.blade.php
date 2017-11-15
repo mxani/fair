f95142fbe9be24d138d19055d86c0a028368f035
parvane <M_pary96@gmail.com>
2017-11-15 07:48:43 +0330

    correction messages content

--- resources/views/productMessage.blade.php
+++ resources/views/productMessage.blade.php
@@ -1,10 +1,12 @@
 @if(!empty($product))
 
 🔹<code>{{$product->title}}</code>
+@if(!empty($product->price))
 
-  👌قیمت :<code>{{$product->price}}</code>
+  💰 قیمت :<code>{{$product->price}}</code> تومان
+@endif
 
-{{$product->description}} ✌️😊
+{{$product->description}}
  
     @isset($product->files[$pic])
         <a href="{{$product->files[$pic]??''}}">&#8205;</a>
